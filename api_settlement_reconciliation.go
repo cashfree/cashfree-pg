d14810bcf34c03a9ea0f65e9e73af26541b2b8d5
bitbucket-pipelines <commits-noreply@bitbucket.org>
2025-04-08 09:54:27 +0000

    Generating Go SDK.

--- api_settlement_reconciliation.go
+++ api_settlement_reconciliation.go
@@ -341,7 +341,7 @@ if _this.XPartnerApiKey != nil {
 	localVarHeaderParams["x-partner-apikey"] = *_this.XPartnerApiKey
 }
 
-	localVarHeaderParams["x-sdk-platform"] = "gosdk-5.0.5"
+	localVarHeaderParams["x-sdk-platform"] = "gosdk-5.0.6"
 	req, err := client.prepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
 	if err != nil {
 		return localVarReturnValue, nil, err
@@ -808,7 +808,7 @@ if _this.XPartnerApiKey != nil {
 	localVarHeaderParams["x-partner-apikey"] = *_this.XPartnerApiKey
 }
 
-	localVarHeaderParams["x-sdk-platform"] = "gosdk-5.0.5"
+	localVarHeaderParams["x-sdk-platform"] = "gosdk-5.0.6"
 	req, err := client.prepareRequest(ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
 	if err != nil {
 		return localVarReturnValue, nil, err
